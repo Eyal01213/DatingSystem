8088f94841e71c1845a0a0687aea18819ee86fcc
Eyal01213 <65185021+Eyal01213@users.noreply.github.com>
2022-07-04 20:47:25 +0300

    upload 2

--- src/main/java/com/john/datingsystem/dtos/User.java
+++ src/main/java/com/john/datingsystem/dtos/User.java
@@ -1,10 +1,58 @@
 package com.john.datingsystem.dtos;
 
+import com.john.datingsystem.Gender;
+
 public class User {
 
     private int id;
     private int age;
+    private Gender gender;
 
 
+    public User() {
+    }
 
+    public User(int id, int age, Gender gender) {
+        this.id = id;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public User(int age, Gender gender) {
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
 }
